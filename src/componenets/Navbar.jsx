fb25465ba445ade0747861c2c85571c6a605257a
cfardin <cfardin51@gmail.com>
2026-07-11 15:24:53 +0600

    added user session and fix navbar

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,40 +1,158 @@
+
+// "use client";
+// import { Moon } from "lucide-react";
+// import { PiBooksFill } from "react-icons/pi";
+// import Link from "next/link";
+// import { authClient } from "@/lib/auth-client";
+
+// const Navbar = () => {
+//     const { data: session } = authClient.useSession();
+
+//     const user = session?.user;
+//     console.log(user);
+
+//     return (
+//         <nav className="flex items-center justify-between px-6 py-3 shadow container mx-auto border-b">
+//             <div className="flex items-center gap-2">
+//                 <Link href="/" className="flex">
+//                     <div className="">
+//                         <PiBooksFill className="" size={30} />
+//                     </div>
+//                     <span className="text-xl font-bold">StudyNook</span>
+//                 </Link>
+//             </div>
+
+//             <div className="flex items-center gap-6">
+//                 <Link href="/">Home</Link>
+//                 <Link href="/all-rooms">Rooms</Link>
+//                 <Link href="/add-rooms">Add Rooms</Link>
+//             </div>
+
+//             <div className="flex items-center gap-4">
+//                 <Moon size={20} className="cursor-pointer" />
+//                 {/* <div>
+//                     <Link href="/login">Login</Link>
+//                     <Link
+//                       href="/register"
+//                       className="bg-gray-900 text-white px-4 py-2 rounded-md"
+//                     >
+//                       Register
+//                     </Link>
+//                 </div> */}
+               
+//             </div>
+
+//             <div className="flex gap-5 navbar-end">
+//                 {/* {user && <h2>Hello {user?.name}</h2>}
+//                 <img
+//                     src={user?.image}
+//                     alt="user pic"
+//                     width={40}
+//                     height={40}
+//                     className="rounded-full"
+//                 ></img> */}
+
+//                 {user ? (
+//                     <>
+//                         <h2>Hello {user?.name}</h2>
+//                         <img
+//                             src={user?.image}
+//                             alt="user pic"
+//                             width={40}
+//                             height={40}
+//                             className="rounded-full"
+//                         ></img>
+//                         <Link
+//                             href={"/login"}
+//                             onClick={async () => await authClient.signOut()}
+//                             className="btn font-bold"
+//                         >
+//                             Log Out
+//                         </Link>
+//                     </>
+//                 ) : (
+//                     <div>
+//                     <Link href="/login">Login</Link>
+//                     <Link
+//                       href="/register"
+//                       className="bg-gray-900 text-white px-4 py-2 rounded-md"
+//                     >
+//                       Register
+//                     </Link>
+//                 </div>
+//                 )}
+//             </div>
+//         </nav>
+//     );
+// };
+
+// export default Navbar;
+
+
+"use client";
 import { Moon } from "lucide-react";
 import { PiBooksFill } from "react-icons/pi";
 import Link from "next/link";
-
+import { authClient } from "@/lib/auth-client";
 
 const Navbar = () => {
-return (
-    <nav className="flex items-center justify-between px-6 py-3 shadow container mx-auto border-b">
-      <div className="flex items-center gap-2">
+    const { data: session } = authClient.useSession();
 
-        <Link href='/' className="flex">
-          <div className="">
-            <PiBooksFill className="" size={30}/>
-          </div>
-          <span className="text-xl font-bold">StudyNook</span>
-        </Link>
-        
-      </div>
- 
-      <div className="flex items-center gap-6">
-        <Link href="/">Home</Link>
-        <Link href="/all-rooms">Rooms</Link>
-        <Link href="/add-rooms">Add Rooms</Link>
-      </div>
- 
-      <div className="flex items-center gap-4">
-        <Moon size={20} className="cursor-pointer" />
-        <Link href="/login">Login</Link>
-        <Link
-          href="/register"
-          className="bg-gray-900 text-white px-4 py-2 rounded-md"
-        >
-          Register
-        </Link>
-      </div>
-    </nav>
-  );
+    const user = session?.user;
+    console.log(user);
+
+    return (
+        <nav className="flex items-center justify-between px-6 py-3 shadow container mx-auto border-b">
+            <div className="flex items-center gap-2">
+                <Link href="/" className="flex">
+                    <div className="">
+                        <PiBooksFill className="" size={30} />
+                    </div>
+                    <span className="text-xl font-bold">StudyNook</span>
+                </Link>
+            </div>
+
+            <div className="flex items-center gap-6">
+                <Link href="/">Home</Link>
+                <Link href="/all-rooms">Rooms</Link>
+                <Link href="/add-rooms">Add Rooms</Link>
+            </div>
+
+            <div className="flex items-center gap-4">
+                <Moon size={20} className="cursor-pointer" />
+
+                {user ? (
+                    <>
+                        <h2>Hello {user?.name}</h2>
+                        <img
+                            src={user?.image}
+                            alt="user pic"
+                            width={40}
+                            height={40}
+                            className="rounded-full"
+                        ></img>
+                        <Link
+                            href={"/login"}
+                            onClick={async () => await authClient.signOut()}
+                            className="bg-gray-900 text-white px-4 py-2 rounded-md"
+                        >
+                            Log Out
+                        </Link>
+                    </>
+                ) : (
+                    <div className="flex items-center gap-4">
+                    <Link href="/login">Login</Link>
+                    <Link
+                      href="/register"
+                      className="bg-gray-900 text-white px-4 py-2 rounded-md"
+                    >
+                      Register
+                    </Link>
+                </div>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
